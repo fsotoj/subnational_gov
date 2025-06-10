07aa01bf2fefe23989cbeb7339fc1816726f0205
Felipe Soto <48763975+fsotoj@users.noreply.github.com>
2025-06-10 18:33:43 +0200

    downloadbutton

--- ui.R
+++ ui.R
@@ -13,12 +13,9 @@ ui <- dashboardPage(
     selectInput("var_sel", "Variable", choices = c("Select a variable",unique(dict$pretty_name)), selected = "Subnat. Leader Sex"),
     box(title = "Variable description", solidHeader = TRUE, width = 12, textOutput("var_description"), collapsible = T, collapsed = T),
     actionButton("apply_filters", "Apply Filters", icon = icon("arrows-rotate")),
-    br(),br(),
-    div(
-      style = "position: absolute; bottom: 20px; width: 100%; padding: 10px;",
-      downloadButton("download_data", "Download complete data"),
-      downloadButton("download_geom", "Download geometries")
-      )
+    br(),
+    downloadButton("download_data", "Download complete data"),
+    downloadButton("download_geom", "Download geometries")
     ),
   dashboardBody(
     tags$head(
@@ -67,7 +64,7 @@ ui <- dashboardPage(
           p("This interactive dashboard provides a comprehensive overview of subnational political leadership across Argentina, Brazil and Mexico. 
     It allows users to explore electoral outcomes, leadership profiles, ideological alignments, and regional trends using dynamic visualizations and maps."),
           
-          p(HTML("The application integrates the Database of Subnational Federalism in Latin America (Giraudy, A. 2025) with the geometries of the federal states and provinces from 
+          p(HTML("The application integrates the Database of Subnational Federalism in Latin America <a href='https://www.agustinagiraudy.com' target='_blank'>(Giraudy, A. 2025)</a> with the geometries of the federal states and provinces from 
          <a href='https://www.simplemaps.com' target='_blank'>www.simplemaps.com</a>. 
          It presents them in an accessible format for researchers, journalists, policymakers, and the general public interested in political dynamics at the subnational level.")),
           
